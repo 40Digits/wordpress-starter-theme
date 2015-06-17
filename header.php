e9a9ed7d540460b2ba302b2a268509f3d48a0550
CK Hicks <ck@40digits.com>
2015-06-17 14:42:40 -0500

    removing duplicate meta theme color

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="theme-color" content="#ee7421">
 
 	<title><?php wp_title('&raquo;','true','right'); ?></title>
 
