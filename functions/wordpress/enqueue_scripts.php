cc3a12f4d2075010857e5be4aefe449e37d89b60
CK Hicks <ck@40digits.com>
2015-06-24 09:25:19 -0500

    updating theme libraries and header.php

--- functions/wordpress/enqueue_scripts.php
+++ functions/wordpress/enqueue_scripts.php
@@ -3,14 +3,19 @@
 function enqueue_custom_styles() {
 
 	// Add main stylesheet
-	wp_register_style('style.css', (get_stylesheet_directory_uri() . '/style.css'), false, false, 'all');
+	wp_register_style('style.css', (get_stylesheet_directory_uri() . '/style.css'), false, false, 'screen');
 	wp_enqueue_style('style.css');
+
+	// Add print stylesheet
+	wp_register_style('print.css', (get_stylesheet_directory_uri() . '/print.css'), false, false, 'print');
+	wp_enqueue_style('print.css');
 }
+add_action( 'wp_enqueue_scripts', 'enqueue_custom_styles' );
 
 function enqueue_custom_scripts() {
 
 	// Add modernizr to the header
-	wp_register_script('modernizr', (get_stylesheet_directory_uri() . '/assets/js/vendor/modernizr.js'), false, '2.8.3', false);
+	wp_register_script('modernizr', (get_stylesheet_directory_uri() . '/assets/js/vendor/modernizr.js'), false, '3.0.0.A.3', false);
 	wp_enqueue_script('modernizr');
 
 	// Remove default jQuery and add Google hosted version to the footer
@@ -24,5 +29,4 @@ function enqueue_custom_scripts() {
 	wp_enqueue_script('site-main');
 
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_custom_styles' );
 add_action( 'wp_enqueue_scripts', 'enqueue_custom_scripts' );
--- header.php
+++ header.php
@@ -8,34 +8,7 @@
 
 	<title><?php wp_title('&raquo;','true','right'); ?></title>
 
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" media="screen" />
-  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/print.css" media="print" />
-
-  <?php // Use http://realfavicongenerator.net/ ?>
-	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/apple-touch-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/apple-touch-icon-180x180.png">
-	<link rel="icon" type="image/png" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/favicon-194x194.png" sizes="194x194">
-	<link rel="icon" type="image/png" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/android-chrome-192x192.png" sizes="192x192">
-	<link rel="icon" type="image/png" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/favicon-16x16.png" sizes="16x16">
-	<link rel="manifest" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/manifest.json">
-	<link rel="shortcut icon" href="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/favicon.ico">
-	<meta name="apple-mobile-web-app-title" content="App Name">
-	<meta name="application-name" content="App Name">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/mstile-144x144.png">
-	<meta name="msapplication-config" content="<?php echo get_bloginfo('template_directory'); ?>/assets/favicons/browserconfig.xml">
-	<meta name="theme-color" content="#ffffff">
-
-	<script>
+  	<script>
 		// Place Google Analytics code here - note that any <noscript> iframes
 		// will break <head>, so include it after <body> if you need that snippet
 
@@ -48,6 +21,11 @@
 		// Empty default Gravity Forms spinner function
 		var gformInitSpinner = function() {};
 	</script>
+
+	<?php
+		// Make sure you go through this file and remove what you aren't using
+		include( get_stylesheet_directory_uri() . '/functions/site/theme-meta.php' );
+	?>
 
 	<?php wp_head(); ?>
 </head>
