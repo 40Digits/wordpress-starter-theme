5996e27ddc7d054bfe81a0b5464a34619efd6f8a
CK Hicks <ck@40digits.com>
2015-07-13 14:33:01 -0500

    adding query counter to defaults

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 	<?php wp_footer(); ?>
 
 	<?php if ( WP_ENV != 'production' ) : ?>
+		<div style="position:fixed;right:0;bottom:0;padding:5px;background:#fff;z-index:99999;"><?php echo get_num_queries(); ?></div>
 		<script type='text/javascript'>
 			(function (d, t) {
 				var bh = d.createElement(t), s = d.getElementsByTagName(t)[0],
