bafc6b145a97806092a78b98260d365ddf9ac3bd
Grant Holle <grant@40digits.com>
2015-07-31 13:04:15 +0800

    Doing work on get_partial_path helper function

--- functions/wordpress/utility.php
+++ functions/wordpress/utility.php
@@ -35,22 +35,38 @@ function wysiwyg_editor_styles() {
 }
 add_action( 'admin_init', 'wysiwyg_editor_styles' );
 
-// This function gets the path to a partial based on keeping the /partials/ standard
-// Allows for use of dot notation, i.e. get_partial_path(sub-dir.component) = theme/partials/sub-dir/component.php
-function get_partial_path($name, $extension = 'php', $partials_dir = '/partials/') {
-	$name = str_replace('.', '/', $name);
+// This function gets the path to a partial based on keeping the `partials` standard
+// Allows for use of dot notation, i.e. get_partial_path('sub-dir.component') would return something like `[...]theme/partials/sub-dir/component.php`
+// Please see [...]/theme/partials/README.md for more examples on usage
+if (!function_exists('get_partial_path')) {
+	function get_partial_path($name, $partials_base_dir = 'partials', $extension = 'php') {
+		// Replace periods in the name with the directory separator if the developer used dot notation
+		$name = str_replace('.', DIRECTORY_SEPARATOR, $name);
 
-	// Quality checks for paths n stuff
-	if ($extension[0] === '.')
-		$extension = substr($extension, 1);
+		// By default, it should just be the extension without the period
+		// If the period was included by accident, remove it
+		if ($extension[0] === '.')
+			$extension = substr($extension, 1);
 
-	if ($partials[0] !== '/')
-		$partials = '/' . $partials;
+		return join_paths(get_template_directory(), $partials_base_dir, $name . '.' . $extension);
+	}
+}
 
-	if ($partials[strlen($partials) - 1] !== '/')
-		$partials = $partials . '/';
+// This function normalizes a filepath, taken from
+// http://stackoverflow.com/questions/1091107/how-to-join-filesystem-path-strings-in-php#answer-15575293
+if (!function_exists('join_paths')) {
+	function join_paths() {
+		$paths = array();
 
-	return get_template_directory() . $partials_dir . $name . '.' . $extension;
+		foreach (func_get_args() as $arg) {
+			$arg = trim($arg);
+
+			if ($arg !== '')
+				$paths[] = $arg;
+		}
+
+		return preg_replace('#' . DIRECTORY_SEPARATOR . '+#', DIRECTORY_SEPARATOR, join(DIRECTORY_SEPARATOR, $paths));
+	}
 }
 
 // Disable WP emoji
