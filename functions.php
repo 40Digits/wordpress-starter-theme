e1ee920b40119d22b10e383fb0e92922c9fb9bfb
Besim Huskic <besim@40digits.com>
2015-07-31 08:00:03 -0600

    move get_partial_path() function to it's own folder as it was getting large

--- functions.php
+++ functions.php
@@ -14,5 +14,8 @@ include_once('functions/wordpress/custom_sidebars.php');
 // Functions (Utility)
 include_once('functions/wordpress/utility.php');
 
+// Partials Include Function
+include_once('functions/site/get-partial-path.php');
+
 // Add CPT icons and seperators to the admin menu
 include_once('functions/wordpress/admin_menu.php');
