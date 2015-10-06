56662a088afae8e389a831dd6bdcd2e4df203ee9
Besim Huskic <besim@40digits.com>
2015-10-06 13:07:02 -0500

    Fix bugs after testing

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 include_once('functions/environment.php');
 
 // Enqueue Scripts
-include_once('functions/wordpress/enqueue_scripts.php');
+include_once('functions/wordpress/enqueue-scripts.php');
 
 // Core Registrations
 include_once('functions/core/custom-post-types.php');
