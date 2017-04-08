9fa70c01baf8b4eaad8762c69ef4db3e532ebf90
CK Hicks <contact@ckhicks.com>
2017-04-08 13:36:51 -0500

    fixing functions paths

--- functions.php
+++ functions.php
@@ -1,9 +1,5 @@
 <?php
 
-// --------------------------------
-// Required
-// --------------------------------
-
 // Environment Management
 include_once('functions/environment.php');
 
@@ -11,22 +7,17 @@ include_once('functions/environment.php');
 include_once('functions/wordpress/enqueue-scripts.php');
 
 // Core Registrations
-include_once('functions/core/custom-post-types.php');
-include_once('functions/core/custom-taxonomies.php');
-// include_once('functions/core/custom-sidebars.php');
-// include_once('functions/core/custom-menus.php');
-// include_once('functions/core/custom-crops.php');
-
-// Partials Include Function
-include_once('functions/wordpress/get-partial-path.php');
-
-
-// --------------------------------
-// Options
-// --------------------------------
+include_once('functions/wordpress/custom-post-types.php');
+include_once('functions/wordpress/custom-taxonomies.php');
+// include_once('functions/wordpress/custom-sidebars.php');
+// include_once('functions/wordpress/custom-menus.php');
+// include_once('functions/wordpress/custom-crops.php');
 
 // Utility Functions
 include_once('functions/wordpress/utility.php');
+
+// Partials Include Function
+// include_once('functions/wordpress/get-partial-path.php');
 
 // Add Admin Menu Seperators
 include_once('functions/wordpress/admin-sidebar-separators.php');
