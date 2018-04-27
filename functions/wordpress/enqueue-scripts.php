ec269ef53c720626b3285a99886674d8127983fc
CK Hicks <contact@ckhicks.com>
2018-04-27 11:51:24 -0500

    Update enqueue-scripts.php

--- functions/wordpress/enqueue-scripts.php
+++ functions/wordpress/enqueue-scripts.php
@@ -10,13 +10,9 @@ function enqueue_custom_scripts() {
 	wp_register_style('style-print', (get_stylesheet_directory_uri() . '/print.css'), false, false, 'print');
 	wp_enqueue_style('style-print');
 
-	// Add modernizr to the header
-	// wp_register_script('modernizr', (get_stylesheet_directory_uri() . '/assets/js/vendor/modernizr.js'), false, '3.0.0.A.3', false);
-	// wp_enqueue_script('modernizr');
-
 	// Remove default jQuery and add Google hosted version to the footer
 	wp_deregister_script('jquery-core');
-	wp_register_script('jquery-core', ('//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'), false, '2.2.4', true);
+	wp_register_script('jquery-core', ('//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'), false, '3.2.1', true);
 	wp_enqueue_script('jquery-core');
 
 	// Load the site's main.js file but make sure jQuery is there first
